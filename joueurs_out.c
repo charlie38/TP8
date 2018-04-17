eccd64dd9d1cff8758e1fcd2a59b23f68ba60206
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-17 15:40:21 +0000

    Add files via upload

---
+++ joueurs_out.c
@@ -0,0 +1,18 @@
+#ifndef __JOUEURS_OUT_H__
+#define __JOUEURS_OUT_H__
+#include <stdio.h>
+#include "joueurs.h"
+
+void ecrire_les_joueurs(joueurs *ens, char *nom_fich) {
+  FILE *fich = fopen(nom_fich,"w");
+  int cardinal = nombre_joueurs(ens);
+  fprintf(fich, "%d\n",cardinal ); /* écrit le nombre de joueurs de l'ensemble sur la première ligne */
+  int i=0;
+  while (i<cardinal){ /* écrit sur chaque ligne un nom de joueur, espace, son nombre de billes */
+    fprintf(fich, "%s %d\n",nom_joueur(ens,i),billes_joueur(ens,i));
+    i++;
+  }
+  fclose(fich);
+}
+
+#endif
