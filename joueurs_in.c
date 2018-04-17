c0e592a9d03158dc56a1f264d56d77805973c39f
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-17 17:44:41 +0000

    Update joueurs_in.c

--- joueurs_in.c
+++ joueurs_in.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include "joueurs.h"
 #include "joueurs_in.h"
@@ -11,8 +12,8 @@ void lire_les_joueurs(joueurs *ens, char *nom_fich){
 	/* on lit la 1ère ligne (cardinal) et on met à jour ens */
 	fscanf(fich,"%d\n",&card);
 	ens->nb = card; 
-	for (int i=0, i<card){ /* on lit le reste du fichier et on complète ens */
-		fscanf(fich,"%s %d\n",&nom,&billes);
+	for (i<card){ /* on lit le reste du fichier et on complète ens */
+		fscanf(fich,"%s %d\n",nom,&billes);
 		strcpy(ens->T[i].pseudo,nom);
 		ens->T[i].nb_billes;
 	}
