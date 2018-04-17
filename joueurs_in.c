20aaa01ef0a33064bc62c49e1ec08d51bb2d55c3
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-17 17:10:01 +0000

    Update joueurs_in.c

--- joueurs_in.c
+++ joueurs_in.c
@@ -1,7 +1,7 @@
 #ifndef __JOUEURS_IN_H__
 #define __JOUEURS_IN_H__
 #include <stdio.h>
-#include "joueurs.h"
+#include "joueurs_in.h"
 
 void lire_les_joueurs(joueurs *ens, char *nom_fich){
 	FILE *fich=fopen(nom_fich,"r");
