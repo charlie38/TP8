3684ea47ddde0d8be7fae77f41470b3b0f124b95
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-17 16:11:29 +0000

    Create joueurs_in.h

---
+++ joueurs_in.h
@@ -0,0 +1,9 @@
+#ifndef __JOUEURS_IN_H__
+#define __JOUEURS_IN_H__
+
+/*
+Lit le contenu d'un ensemble de joueurs dans un fichier 
+*/
+void lire_les_joueurs(joueurs *ens, char *nom_fich);
+
+#endif
