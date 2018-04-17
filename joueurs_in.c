0ef6322d6164bd5bacaa6aadbdcb098abc7e626c
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-17 17:04:15 +0000

    Update joueurs_in.c

--- joueurs_in.c
+++ joueurs_in.c
@@ -1,20 +1,21 @@
 #ifndef __JOUEURS_IN_H__
 #define __JOUEURS_IN_H__
+#include <stdio.h>
 #include "joueurs.h"
 
 void lire_les_joueurs(joueurs *ens, char *nom_fich){
-	FILE *fich=fopen(nom_fich;"r") ;
-	int card ;
-	char *nom ;
-	int billes ;
+	FILE *fich=fopen(nom_fich,"r");
+	int card;
+	char *nom;
+	int billes;
 
 	/* on lit la 1ère ligne (cardinal) et on met à jour ens */
-	fscanf(fich,"%d\n",&card) ;
-	ens->nb = card ; 
+	fscanf(fich,"%d\n",&card);
+	ens->nb = card; 
 	for (int i=0, i<card){ /* on lit le reste du fichier et on complète ens */
-		fscanf(fich,"%s %d\n",&nom,&billes) ;
-		strcpy(ens->T[i].pseudo,nom) ;
-		ens->T[i].nb_billes ;
+		fscanf(fich,"%s %d\n",&nom,&billes);
+		strcpy(ens->T[i].pseudo,nom);
+		ens->T[i].nb_billes;
 	}
 }
 #endif
