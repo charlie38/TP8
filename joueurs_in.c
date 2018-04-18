8f4ae4acd127271116ab39d8461317b21e5448c2
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-18 08:29:45 +0000

    Update joueurs_in.c

--- joueurs_in.c
+++ joueurs_in.c
@@ -5,7 +5,6 @@
 
 void lire_les_joueurs(joueurs *ens, char *nom_fich){
 	FILE *fich=fopen(nom_fich,"r");
-	int i=0;
 	int card;
 	char *nom;
 	int billes;
@@ -13,9 +12,9 @@ void lire_les_joueurs(joueurs *ens, char *nom_fich){
 	/* on lit la 1ère ligne (cardinal) et on met à jour ens */
 	fscanf(fich,"%d\n",&card);
 	ens->nb = card; 
-	for (i<card){ /* on lit le reste du fichier et on complète ens */
+	for (int i=0;i<card;i++){ /* on lit le reste du fichier et on complète ens */
 		fscanf(fich,"%s %d\n",nom,&billes);
 		strcpy(ens->T[i].pseudo,nom);
-		ens->T[i].nb_billes;
+		ens->T[i].nb_billes = billes;
 	}
 }
